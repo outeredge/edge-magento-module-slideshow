d45e302439f05cabc62433fedd334a9c9e285f68
Bruno Dias <bruno@outeredgeuk.com>
2015-07-09 15:15:56 +0100

    Add allowed permissions

--- app/code/local/Edge/Slideshow/controllers/Adminhtml/SlideshowController.php
+++ app/code/local/Edge/Slideshow/controllers/Adminhtml/SlideshowController.php
@@ -2,6 +2,11 @@
 
 class Edge_Slideshow_Adminhtml_SlideshowController extends Mage_Adminhtml_Controller_Action
 {
+    protected function _isAllowed()
+    {
+        return Mage::getSingleton('admin/session')->isAllowed('cms/slideshow');
+    }
+    
     protected function _initAction()
     {
         $this->loadLayout()
