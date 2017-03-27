b77e1bf2895d7f6a652b823336631cb0d814dc00
broopdias <ibroopi@gmail.com>
2017-03-27 10:10:20 +0100

    Change sql query to check if table already exist
    Change sql query to check if table already exist

--- app/code/local/Edge/Slideshow/sql/slideshow_setup/mysql4-upgrade-1.0.0-1.0.1.php
+++ app/code/local/Edge/Slideshow/sql/slideshow_setup/mysql4-upgrade-1.0.0-1.0.1.php
@@ -1,10 +1,16 @@
 <?php
-$installer = $this;
-$installer->startSetup();
 
-$installer->run("
-    ALTER TABLE {$this->getTable('slideshow/slideshow')}
-        ADD `from_date` datetime NULL default NULL,
-        ADD `to_date` datetime NULL default NULL;
-    ");
-$installer->endSetup();
+$this->startSetup();
+$this->getConnection()->addColumn(
+    $this->getTable('slideshow/slideshow'),
+    'from_date',
+    'datetime NULL default NULL'
+);
+
+$this->getConnection()->addColumn(
+    $this->getTable('slideshow/slideshow'),
+    'to_date',
+    'datetime NULL default NULL'
+);
+
+$this->endSetup();
